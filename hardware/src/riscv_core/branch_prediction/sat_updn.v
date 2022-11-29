641928e4cb2e33b5297da6e9c55e2ea44d7c0f61
AMKS <arushisomani@berkeley.edu>
2022-11-29 14:38:06 -0800

    saturating counter start

--- hardware/src/riscv_core/branch_prediction/sat_updn.v
+++ hardware/src/riscv_core/branch_prediction/sat_updn.v
@@ -13,7 +13,52 @@ module sat_updn #(
     output [WIDTH-1:0] out
 );
 
-    // TODO: Your code
-    assign out = '0;
+    reg [WIDTH-1:0] out_reg;
+    always @(*) begin
+        // Branch taken, zero strikes.
+        if (in == 'b10) begin
+            if (down)           // Guessed taken, was not taken.
+                out = 'b11;     // Add strike.
+            else if (up)        // Guessed taken, was taken.
+                out = 'b10;     // Stay in same state.
+            else                // Should be unreachable.
+                out = 'b10;
+        end
+
+        // Branch taken, one strike.
+        else if (in == 'b11) begin
+            if (down)           // Guessed taken, was not taken.
+                out = 'b01;     // Toggle to guessing not taken.
+            else if (up)        // Guessed taken, was taken.
+                out = 'b10;     // Remove strike.
+            else                // Should be unreachable.
+                out = b'11;
+        end
+
+        // Branch not taken, zero strikes.
+        else if (in == 'b01) begin
+            if (down)           // Guessed not taken, was not taken.
+                out = 'b01;     // Stay in state.
+            else if (up)        // Guessed not taken, was taken.
+                out = 'b00;     // Add strike.
+            else                // Should be unreachable.
+                out = 'b00;
+        end
+
+        // Branch not taken, one strike.
+        else if (in == 'b00) begin
+            if (down)           // Guessed not taken, was not taken.
+                out = 'b01;     // Remove strike.
+            else if (up)        // Guessed not taken, was taken.
+                out = 'b10;     // Toggle to guessing taken.
+            else                // Should be unreachable.
+                out = b'00;
+        end
+
+        // Unreachable
+        else begin
+            out = 'b10;
+        end
+    end
 
 endmodule
