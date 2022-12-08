8a819a74d16156c4fb371d7f63bdfe305fb237e2
AMKS <arushisomani@berkeley.edu>
2022-12-08 15:54:49 -0800

    wb sel

--- hardware/src/riscv_core/wb_selector.v
+++ hardware/src/riscv_core/wb_selector.v
@@ -1,7 +1,5 @@
 module wb_selector(
-    input [31:0] mem_bios_dout,
-    input [31:0] dmem_lex,
-    input [31:0] uart_out,
+    input [31:0] out_lex,
     input [31:0] pc,
     input [31:0] alu,
     input [1:0] wb_sel,
@@ -13,12 +11,7 @@ module wb_selector(
         if (wb_sel == 2) begin
             wb_val = pc + 4;
         end else if (wb_sel == 1) begin
-            // If the top four bits of the address are 0100, we are reading from BIOS. Otherwise, DMEM.
-            case (mem_sel)
-                2'b10: wb_val = mem_bios_dout;
-                2'b01: wb_val = uart_out;
-                default: wb_val = dmem_lex;
-            endcase
+            wb_val = out_lex;
         end else begin
             wb_val = alu;
         end
